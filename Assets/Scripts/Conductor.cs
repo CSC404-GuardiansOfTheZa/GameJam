f697649f5653238c5d1eedfd97df662fa4669b8a
Stew Esho <stewardesho@gmail.com>
2022-02-05 17:28:03 -0500

    Added proper beatkeeping in Conductor (next step is to sync jumping/events to it

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -3,35 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(AudioSource))]
 public class Conductor : MonoBehaviour
 {
+    [Header("NOTE: Add music track HERE, not at AudioSource")]
+    [SerializeField]
+    private AudioClip musicTrack;
 
     [field: SerializeField]
     public int BPM {get; private set;}
     [field: SerializeField]
-    public float trackLengthInSeconds {get; private set;}
+    public float TrackLengthInSeconds {get; private set;}
     [field: SerializeField]
-    public float trackOffsetInSeconds {get; private set;}
-    [SerializeField]
-    private AudioClip musicTrack;
+    public float TrackOffsetInSeconds {get; private set;}
 
     public float Crotchet { // length of a beat
-        get {return 60 / BPM; }
+        get; private set;
     }
-    public float SongPosition { // in seconds
+    public double SongPosition { // in seconds
         get {
             if (this.dspTimeStart < 0) {
                 return 0.0f;
             }
-            return (float)AudioSettings.dspTime - dspTimeStart;
+            return AudioSettings.dspTime - dspTimeStart;
         }
     }
 
-    private float dspTimeStart = -1.0f;
+    private double dspTimeStart = -1.0f;
+    private AudioSource asource;
     
     [Space(10)]
     [Header("DEBUG")]
     [SerializeField]
     private bool placeholder = false;
+
+    private int beat = 1;
+
+    void Awake() {
+        asource = GetComponent<AudioSource>();
+        asource.clip = this.musicTrack;
+        Crotchet = 60.0f / (float) BPM;
+    }
+
+
+    double lastBeat = 0; 
+    void Start() {
+        asource.Play();
+        dspTimeStart = AudioSettings.dspTime;
+        Debug.Log(Crotchet);
+    }
+
+    
+    void Update() {
+        if (SongPosition > Crotchet * beat) {
+            Debug.Log(((beat-1)%4) + 1);
+            beat += 1;
+        }
+    }
 
 }
