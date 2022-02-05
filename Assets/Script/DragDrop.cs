c594197f37d18a2d307b908e5cf64371b9b8099f
Keying Chen <35384984+KKyInGG@users.noreply.github.com>
2022-02-05 16:58:41 -0500

    Change ManHole Behavior

--- Assets/Script/DragDrop.cs
+++ Assets/Script/DragDrop.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class DragDrop : MonoBehaviour
 {
-    private Vector3 _cursorPos;
+
     private Camera _cam;
-    private GameObject _selectedObject;
-    private Vector3 _originalPos;
-    private string destinationTag = "DropArea";
+
+    [SerializeField] private Animator _myAnimationController;
     // Start is called before the first frame update
     void Start()
     {
         _cam = Camera.main;
-        _selectedObject = null;
+        _myAnimationController = GetComponent<Animator>();
 
     }
 
@@ -30,64 +29,20 @@ public class DragDrop : MonoBehaviour
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            // left click with no object selected
-            if (_selectedObject == null)
+            if (Physics.Raycast(ray, out hit, 100f))
             {
-                if (Physics.Raycast(ray, out hit, 100f))
+                if (hit.transform != null)
                 {
-                    if (hit.transform != null)
-                    {
-                        if (hit.collider.CompareTag("Drag_Cover"))
-                        {
-                            _originalPos = hit.collider.gameObject.transform.position;
-                            _selectedObject = hit.collider.gameObject;
-                            Cursor.visible = false;
-                        }
-                        else
-                        {
-                            return;
-                        }
-                    }
-                }
-            }
-            // left click when object selected
-            else
-            {
-
-                Vector3 mousePosition = GetMousePosition();
-                var rayDirection = mousePosition - _cam.transform.position;
-                RaycastHit hitObject;
-                if(Physics.Raycast(mousePosition, rayDirection, out hitObject))
-                {
-                    if(hitObject.transform.tag == destinationTag)
-                    {
-                        _selectedObject.transform.position = hitObject.transform.position;
-                        _selectedObject.tag = "UnDraggable";
+                    if (hit.collider.CompareTag("Drag_Cover"))
+                    { 
+                        _myAnimationController.SetTrigger("MoveManHole");
                     }
                     else
                     {
-                        _selectedObject.transform.position = _originalPos;
+                        return;
                     }
                 }
-                _selectedObject = null;
-                Cursor.visible = true;
             }
         }
-        // no click when object selected (move with mouse)
-        if (_selectedObject != null)
-        {
-            _selectedObject.transform.position = GetMousePosition();
-
-        }
-    }
-
-    Vector3 GetMousePosition()
-    {
-        Vector3 mousePosition = _cam.ScreenToWorldPoint(new Vector3(
-               Input.mousePosition.x,
-               Input.mousePosition.y,
-               Mathf.Abs(_cam.transform.position.z)
-           ));
-        return mousePosition;
     }
 }
