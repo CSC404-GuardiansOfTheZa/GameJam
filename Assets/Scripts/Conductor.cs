f1f7979f5869342cd136bc85aa96fe620f93373d
Stew Esho <stewardesho@gmail.com>
2022-02-05 16:54:27 -0500

    added more vars to conductor

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Conductor : MonoBehaviour
 {
@@ -9,11 +10,28 @@ public class Conductor : MonoBehaviour
     public int BPM {get; private set;}
     [field: SerializeField]
     public float trackLengthInSeconds {get; private set;}
+    [field: SerializeField]
+    public float trackOffsetInSeconds {get; private set;}
     [SerializeField]
     private AudioClip musicTrack;
 
     public float Crotchet { // length of a beat
         get {return 60 / BPM; }
     }
+    public float SongPosition { // in seconds
+        get {
+            if (this.dspTimeStart < 0) {
+                return 0.0f;
+            }
+            return (float)AudioSettings.dspTime - dspTimeStart;
+        }
+    }
+
+    private float dspTimeStart = -1.0f;
+    
+    [Space(10)]
+    [Header("DEBUG")]
+    [SerializeField]
+    private bool placeholder = false;
 
 }
