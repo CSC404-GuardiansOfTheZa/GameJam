2dcd29a3b5632d59170c222ae729d8498e12e672
Stew Esho <stewardesho@gmail.com>
2022-02-09 14:14:21 -0500

    refactored conductor

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -9,6 +9,8 @@ public class Conductor : MonoBehaviour
     private static Conductor _instance;
     public static Conductor Instance { get { return _instance; } } 
 
+    public delegate void onBeatDelegate(int beatNum);
+    public event onBeatDelegate onBeat;
 
     [Header("NOTE: Add music track HERE, not at AudioSource")]
     [SerializeField] private AudioClip musicTrack;
@@ -29,13 +31,9 @@ public class Conductor : MonoBehaviour
         }
     }
 
-    private double dspTimeStart = -1.0f;
     private AudioSource asource;
-    
-    [Space(10)]
-    [Header("DEBUG")]
-
-    private int beat = 1;
+    private double dspTimeStart = -1.0f;
+    private int beat = 1; 
 
     void Awake() {
         if (_instance != null && _instance != this) {
@@ -48,16 +46,11 @@ public class Conductor : MonoBehaviour
         asource.clip = this.musicTrack;
         Crotchet = 60.0f / (float) BPM;
     }
-
-    double lastBeat = 0; 
     void Start() {
         asource.Play();
         dspTimeStart = AudioSettings.dspTime;
         Debug.Log(Crotchet);
     }
-
-    public delegate void onBeatDelegate(int beatNum);
-    public event onBeatDelegate onBeat;
     
     void Update() {
         if (SongPosition > Crotchet * beat && onBeat != null) {
