5766f2a8cc389fb8c230b7bb3ec6a79ac97270db
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2022-02-13 02:23:40 -0500

    save and cleanup

--- Assets/Scripts/AngelController.cs
+++ Assets/Scripts/AngelController.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class AngelController : MonoBehaviour {
 
-    private Camera _cam;
-
     public float smoothTime = 5f;
-    private Vector3 velocity = Vector3.zero;
     public float rotateSpeed = 10f;
 
-    public Transform model;
+    private Camera _cam;
+    private Vector3 velocity = Vector3.zero;
 
     void Start() {
         _cam = Camera.main;
@@ -26,28 +24,8 @@ public class AngelController : MonoBehaviour {
             )
         );
 
-
         transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime * Time.deltaTime);
-
-        if (velocity.x < 0) {
-            model.eulerAngles = new Vector3(model.eulerAngles.x, Mathf.Lerp(model.eulerAngles.y, 90, rotateSpeed * Time.deltaTime), 0);
-        } else {
-            model.eulerAngles = new Vector3(model.eulerAngles.x, Mathf.Lerp(model.eulerAngles.y, 270, rotateSpeed * Time.deltaTime), 0);
-        }
-
-
-
-
-        // Vector3 targetDirection = target - transform.position;
-        // float singleStep = rotateSpeed * Time.deltaTime;
-        // // Rotate the forward vector towards the target direction by one step
-        // Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-        // // Draw a ray pointing at our target in
-        // Debug.DrawRay(transform.position, newDirection, Color.red);
-        // // Calculate a rotation a step closer to the target and applies rotation to this object
-        // transform.rotation = Quaternion.LookRotation(new Vector3(transform.eulerAngles.x, newDirection.y, 0));
-
-        print(velocity);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Lerp(transform.eulerAngles.y, velocity.x < 0 ? 90 : 270, rotateSpeed * Time.deltaTime), 0);
     }
 
 }
