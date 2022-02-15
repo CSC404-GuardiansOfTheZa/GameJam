a0ca266b528a51800e6736e13f3128731edfb0f9
Stew Esho <stewardesho@gmail.com>
2022-02-15 12:15:04 -0500

    Bird now waits for duration before leaving

--- Assets/Scripts/Interactables/Bird/Bird.cs
+++ Assets/Scripts/Interactables/Bird/Bird.cs
@@ -8,9 +8,10 @@ public class Bird : MonoBehaviour, IInteractable {
 	[SerializeField] private Vector3 relativeStartPos = new Vector3(-40, 50, 0); // Where to move to when beginning hover animation; relative to pizza guy's coords
 	[SerializeField] private float hoverDistance = 2.5f; // # of units to hover over the ground 
 	[SerializeField] private float hoverDuration = 10f; // # of seconds to hover over the pizza guy, if not shoo'd
-	[SerializeField] private int framesToBeginHover = 60; // # of frames from trigger to hovering.
+	[SerializeField] private int framesToBeginHover = 60; // # of frames from trigger to hovering. TODO: change to seconds
 
 	private Collider col;
+	private bool isShooed;
 
 	private void Awake() {
 		this.col = this.GetComponent<Collider>();
@@ -40,6 +41,13 @@ public class Bird : MonoBehaviour, IInteractable {
 			transform.position = new Vector3(x, y, vertexPt.z);
 			yield return null;
 		}
+
+		StartCoroutine(this.Hover());
+	}
+
+	IEnumerator Hover() {
+		yield return new WaitForSeconds(this.hoverDuration);
+		if (!this.isShooed) this.Trigger();
 	}
 
 	IEnumerator EndHover(float newHeight) {
@@ -55,10 +63,10 @@ public class Bird : MonoBehaviour, IInteractable {
 		this.gameObject.SetActive(false);
 	}
 
-
 	public void Trigger() {
 		// Shoo away the Bird
 		// Disable colider 
+		this.isShooed = true;
 		this.col.enabled = false;
 		
 		StartCoroutine(EndHover(10f)); // TODO: turn this magic number into a SerializeField
