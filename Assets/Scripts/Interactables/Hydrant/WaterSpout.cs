3a4c9ba160e958a616a59ab38c77b80ca851d554
Stew Esho <stewardesho@gmail.com>
2022-02-15 16:22:47 -0500

    Added hydrant toggling

---
+++ Assets/Scripts/Interactables/Hydrant/WaterSpout.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSpout : MonoBehaviour {
+    [SerializeField] private float secondsToExtend = 1.0f;
+    [SerializeField] private bool startExtended = false;
+
+    public float SpoutHeight { get; set; } = -1;
+
+    private bool extended;
+    
+    public void Awake() {
+        this.extended = this.startExtended;
+        SpoutHeight = transform.localScale.y;
+        SetYScale(this.extended ? SpoutHeight : 0);
+    }
+
+    public void ToggleSpout() {
+        extended = !this.extended;
+        StartCoroutine(ExtendTo(extended ? SpoutHeight : 0));
+    }
+
+    IEnumerator ExtendTo(float height) {
+        float elapsedTime = 0.0f;
+        while (elapsedTime < this.secondsToExtend) {
+            float progress = elapsedTime / this.secondsToExtend;
+            float newHeight = Mathf.Lerp(transform.localScale.y, height, progress);
+            SetYScale(newHeight);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void SetYScale(float yScale) {
+        // When setting scale, also set y position so it only scales from the top (instead of from the center)
+        Vector3 scale = transform.localScale;
+        scale.y = yScale;
+        transform.localScale = scale;
+        
+        Vector3 pos = transform.localPosition;
+        pos.y = yScale;
+        transform.localPosition = pos;
+    }
+}
