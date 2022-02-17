f6299582d6da55f1cda11930e9a3fabe2ff835d0
Keying Chen <35384984+KKyInGG@users.noreply.github.com>
2022-02-17 18:40:12 -0500

    Add DeathObject to Manhole

--- Assets/kk_Script/MoveTest.cs
+++ Assets/kk_Script/MoveTest.cs
@@ -6,17 +6,25 @@ public class MoveTest : MonoBehaviour
 {
     // Start is called before the first frame update
     private float _userHorizontalInput;
+    private float _userVerticalInput;
     private const float ScaleMovment = 0.25f;
+    private Transform trans;
+    private Vector3 _rot;
+
     void Start()
     {
-        
+        trans = gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         _userHorizontalInput = Input.GetAxis("Vertical");
-        gameObject.GetComponent<Transform>().position += transform.forward * _userHorizontalInput * ScaleMovment;
+        _userVerticalInput = Input.GetAxis("Horizontal");
+        _rot = trans.rotation.eulerAngles;
+        _rot += new Vector3(0, _userVerticalInput, 0);
+        trans.rotation = Quaternion.Euler(_rot);
+        trans.position += transform.forward * _userHorizontalInput * ScaleMovment;
 
     }
 }
