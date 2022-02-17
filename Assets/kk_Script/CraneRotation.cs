28747bdda73b8586f1a788f0ee04c2fd75eed55f
Stew Esho <stewardesho@gmail.com>
2022-02-17 11:55:15 -0500

    hooked crane into IInteractable interface

--- Assets/kk_Script/CraneRotation.cs
+++ Assets/kk_Script/CraneRotation.cs
@@ -2,39 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CraneRotation : MonoBehaviour
-{
-	private Transform _trans;
-	private float lerpTime = 1;
+public class CraneRotation : MonoBehaviour, IInteractable {
+	[SerializeField] private float rotateTime = 0.5f;
 	private bool rotate = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _trans = GetComponent<Transform>();
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    	if(Input.GetMouseButtonDown(0) && !rotate){
-    		rotate = true;
-    		StartCoroutine(RotateMe(Vector3.up * (-180), 5f));
-    	}        
+    public void Trigger() {
+	    if (!rotate) {
+		    this.rotate = true;
+    		StartCoroutine(RotateMe(Vector3.up * (-180), this.rotateTime));
+	    }
     }
 
     IEnumerator RotateMe(Vector3 byAngles, float inTime)
      {
-         var fromAngle = _trans.rotation;
-         var toAngle = Quaternion.Euler(_trans.eulerAngles + byAngles);
+         var fromAngle = transform.rotation;
+         var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
          for (var t = 0f; t <= 1; t += Time.deltaTime / inTime)
          {
-             _trans.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+             transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
            
              yield return null;
          }
  
-         _trans.rotation = toAngle;
+         transform.rotation = toAngle;
          rotate = false;
      }
 
