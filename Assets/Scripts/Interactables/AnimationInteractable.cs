e46f928a7a4b9429a1f4db12a7f795676c46e9e3
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2022-02-16 21:35:22 -0500

    particle when activated window

--- Assets/Scripts/Interactables/AnimationInteractable.cs
+++ Assets/Scripts/Interactables/AnimationInteractable.cs
@@ -2,24 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationInteractable : MonoBehaviour, IInteractable
-{
+public class AnimationInteractable : MonoBehaviour, IInteractable {
     [SerializeField] protected string animationName = "";
 
-    protected Animator animator;    
+    protected Animator animator;
+    protected ParticleSystem particles;
 
     void Awake() {
         animator = GetComponent<Animator>();
+        particles = GetComponentInChildren<ParticleSystem>();
     }
 
     public void Trigger() {
-        if (animator){
+
+
+        if (animator) {
             animator.SetTrigger(animationName);
         }
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         else {
             Debug.LogError("Interactable animation was triggered, but no Animator was set");
         }
-        #endif
+
+        particles?.Play();
+
+
+#endif
     }
 }
