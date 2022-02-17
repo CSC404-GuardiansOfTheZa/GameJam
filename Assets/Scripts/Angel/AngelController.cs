e3b787c9e6d66206ea62035e1de0d10a805ea3bb
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2022-02-16 20:53:40 -0500

    angel interactable

--- Assets/Scripts/Angel/AngelController.cs
+++ Assets/Scripts/Angel/AngelController.cs
@@ -8,19 +8,47 @@ public class AngelController : MonoBehaviour {
     [SerializeField] private float smoothTime = 5f;
     [SerializeField] private float rotateSpeed = 10f;
     [Header("Leaning")]
-    [SerializeField] private float speedForMaxLean = 50f; // When abs(velocity.x) reaches speedForMaxLean, the angel should then reach the max lean angle (specified below)
+    [SerializeField] private float maxLeanSpeed = 50f; // When abs(velocity.x) reaches speedForMaxLean, the angel should then reach the max lean angle (specified below)
     [SerializeField] private float maxLeanAngle = 80f; // Angel can not lean more than maxLeanAngle degrees at full speed
 
     private Camera cam;
     private Vector3 velocity = Vector3.zero;
-    
+
     private float lean = 0.0f;
+
+    private GameObject selectedInteractable = null;
 
     void Start() {
         this.cam = Camera.main;
     }
 
     void Update() {
+
+        // detect interactables in vicinity
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, 0.5f, Vector3.forward);
+        bool hasInteractable = false;
+        foreach (var hit in hits) {
+            if (!hit.collider.TryGetComponent<IInteractable>(out IInteractable _))
+                continue;
+            hasInteractable = true;
+            if (selectedInteractable == hit.collider.gameObject) // same gameobject, do nothing
+                continue;
+            selectedInteractable?.GetComponent<Outline>()?.HideOutline();
+            hit.collider.GetComponent<Outline>()?.ShowOutline();
+            selectedInteractable = hit.collider.gameObject;
+            print(hit.collider.name);
+        }
+
+        if (!hasInteractable) {
+            selectedInteractable?.GetComponent<Outline>()?.HideOutline();
+            selectedInteractable = null;
+        }
+
+
+        // controls for interactable
+        if (Input.GetMouseButtonDown(0))
+            selectedInteractable?.GetComponent<IInteractable>().Trigger();
+
         Vector3 target = this.cam.ScreenToWorldPoint(
             new Vector3(
                 Input.mousePosition.x,
@@ -29,12 +57,27 @@ public class AngelController : MonoBehaviour {
             )
         );
 
+
         transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime * Time.deltaTime);
         transform.eulerAngles = new Vector3(
-            -Mathf.Abs(this.velocity.x) / this.speedForMaxLean * this.maxLeanAngle,
-            Mathf.Lerp(transform.eulerAngles.y, velocity.x < 0 ? 90 : 270, rotateSpeed * Time.deltaTime), 
+            -Mathf.Abs(this.velocity.x) / this.maxLeanSpeed * this.maxLeanAngle,
+            Mathf.Lerp(transform.eulerAngles.y, velocity.x < 0 ? 90 : 270, rotateSpeed * Time.deltaTime),
             0);
-        
+
     }
+
+    // void OnTriggerEnter(Collider collider) {
+    //     if (!collider.TryGetComponent<IInteractable>(out IInteractable _))
+    //         return;
+    //     selectedInteractable?.GetComponent<Outline>()?.HideOutline(); // disable outline of prev selected interactable
+    //     selectedInteractable = collider.gameObject;
+    //     collider.GetComponent<Outline>()?.ShowOutline();
+    // }
+
+    // void OnTriggerExit(Collider collider) {
+    //     collider.GetComponent<Outline>()?.HideOutline();
+    //     if (selectedInteractable == collider.gameObject)
+    //         selectedInteractable = null;
+    // }
 
 }
