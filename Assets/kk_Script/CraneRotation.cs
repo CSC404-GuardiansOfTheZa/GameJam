0a88a604cde33a00b671ac71e221e40005fb407e
Keying Chen <35384984+KKyInGG@users.noreply.github.com>
2022-02-17 02:16:51 -0500

    CraneRotation

---
+++ Assets/kk_Script/CraneRotation.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneRotation : MonoBehaviour
+{
+	private Transform _trans;
+	private float lerpTime = 1;
+	private bool rotate = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _trans = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(Input.GetMouseButtonDown(0) && !rotate){
+    		rotate = true;
+    		StartCoroutine(RotateMe(Vector3.up * (-180), 0.5f));
+    	}        
+    }
+
+    IEnumerator RotateMe(Vector3 byAngles, float inTime)
+     {
+         var fromAngle = _trans.rotation;
+         var toAngle = Quaternion.Euler(_trans.eulerAngles + byAngles);
+         for (var t = 0f; t <= 1; t += Time.deltaTime / inTime)
+         {
+             _trans.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+           
+             yield return null;
+         }
+ 
+         _trans.rotation = toAngle;
+         rotate = false;
+     }
+
+}
