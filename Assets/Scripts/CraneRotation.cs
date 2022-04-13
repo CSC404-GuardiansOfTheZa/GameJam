a3a99f3fb35815dbfc2b57e08e48cbd5e982b64f
Stew Esho <stewardesho@gmail.com>
2022-04-13 16:40:59 -0400

    interactables reset on death!

--- Assets/Scripts/CraneRotation.cs
+++ Assets/Scripts/CraneRotation.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CraneRotation : Interactable
-{
+public class CraneRotation : Interactable {
+    private Vector3 startRotation;
+
+    public void Start() {
+        this.startRotation = transform.parent.eulerAngles;
+        LevelManager.Instance.OnLevelReload += delegate { transform.parent.eulerAngles = this.startRotation; };
+    }
+
     IEnumerator RotateMe(Vector3 byAngles, float inTime, Transform tnsfm)
      {
          var fromAngle = tnsfm.rotation;
